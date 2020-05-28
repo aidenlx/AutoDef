f59f4c8363aac2d825bd50117ee1a5096abb0385
aidenlx <aiden.lx@outlook.com>
2020-05-28 11:56:03 +0800

    Ver.1.0，实现点击笔记更新标题,优化代码结构

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-const formatText = function(text)
-{
-  const chs = RegExp(/[\u4e00-\u9fa5]/g);
-  text=text.split('\n').filter(e=>
-((e.match(/\b/g)||0).length||0)/2+((e.match(chs)||0).length||0)<8
-  ).join('\n');//去除长句
-  text=text.replace(/[.,?!+·"。，；？！—“”:：]/g,'');
-  text=text.replace(/[、()（）]|又?称为?|或者?|即/g,'\n');
-  text=text.replace(/ {2,}/g,' ');
-  text=text.replace(/ *- */g,'');
-  text.replace(/([A-Za-z]+)[( ]or ([A-Za-z]+) ([A-Za-z]+?(?=$))/gm,'$1 $3\n$2 $3;')
-  let group=text.split('\n').filter(e => e);//去除空值  
-  group.forEach(function(text) {
-   text = text.replace(/^ +| +$/g,'');
-  });
-  group=Array.from(new Set(group));//es6新特性数组去重
-  group.sort(function(a,b)
+const defLimit=10;
+const chs = RegExp(/[\u4e00-\u9fa5]/g);
+const chsFisrt = function(a,b)
   {
     let at=chs.test(a);
     let bt=chs.test(b);
@@ -27,9 +13,48 @@ const formatText = function(text)
       return 1;
     return 0;
     }
-  });
-  return group.join(';');
+  }
+const toTitleCase = (phrase) => {
+  return phrase
+    .toLowerCase()
+    .split(/\b/g)
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join('');
 };
+String.prototype.getWordCount=function()
+{
+  return ((this.match(/\b/g)||0).length||0)/2+((this.match(chs)||0).length||0)
+}
+const getGroupedDef = function(text,isTitle)
+{
+  if (text)
+  { 
+    text=text.toLowerCase();
+    text=isTitle?text:text.split("\n").filter(e=>e.getWordCount()<defLimit).join("\n");//去除长句
+    text=text.replace(/[.,?!+·"。，；？！—“”:：]/g,'');
+    text=text.replace(/[、()（）]|又?称为?|或者?|即/g,'\n');
+    text=text.replace(/ {2,}/g,' ');
+    text=text.replace(/ *- */g,'');
+    text=text.replace(/^ +| +$/gm,'');//去除开头与结尾的多余空格
+    text.replace(/([A-Za-z]+)[( ]or ([A-Za-z]+) ([A-Za-z]+?(?=$))/gm,'$1 $3\n$2 $3;')
+    return text.split(/[\n;]/g);//拆分行及原有的分词
+  }
+  else
+    return null;
+};
+//输入含有GroupedDef的Array
+const formatText = function(...groups)
+{
+  var group0=[];
+  for (var g of groups) {
+    group0=group0.concat(g);
+  }
+  group0=Array.from(new Set(group0));//es6新特性数组去重
+  group0=group0.filter(e => e);//去除空值
+  group0.sort(chsFisrt);
+  return toTitleCase(group0.join(';'));
+}
+
 
 JSB.newAddon = function(mainPath){
   let newAddonClass = JSB.defineClass('AutoDef : JSExtension', /*Instance members*/{
@@ -47,15 +72,19 @@ JSB.newAddon = function(mainPath){
     sceneDidBecomeActive: function() {
     },
     notebookWillOpen: function(notebookid) {
-      NSNotificationCenter.defaultCenter().addObserverSelectorName(self,'onProcessNewExcerpt:','ProcessNewExcerpt');
-      NSNotificationCenter.defaultCenter().addObserverSelectorName(self,'onProcessExcerptChange:','ChangeExcerptRange');
-      //NSNotificationCenter.defaultCenter().addObserverSelectorName(self, 'onPopupMenuOnNote:', 'PopupMenuOnNote');
+      //NSNotificationCenter.defaultCenter().addObserverSelectorName(self,'onProcessNewExcerpt:','ProcessNewExcerpt');
+      //NSNotificationCenter.defaultCenter().addObserverSelectorName(self,'onProcessExcerptChange:','ChangeExcerptRange');
+      NSNotificationCenter.defaultCenter().addObserverSelectorName(self, 'onPopupMenuOnNote:', 'PopupMenuOnNote');
       self.autodef = NSUserDefaults.standardUserDefaults().objectForKey('marginnote_autodef');
+      self.switchtitle = NSUserDefaults.standardUserDefaults().objectForKey('marginnote.extension.switchtitle');
+      self.autotitle = NSUserDefaults.standardUserDefaults().objectForKey('marginnote.extension.autotitle');
+      self.autotitle_with_excerpt = NSUserDefaults.standardUserDefaults().objectForKey('marginnote.extension.autotitle_with_excerpt');
+
     },
     notebookWillClose: function(notebookid) {
-      NSNotificationCenter.defaultCenter().removeObserverName(self,'ProcessNewExcerpt');
-      NSNotificationCenter.defaultCenter().removeObserverName(self,'ChangeExcerptRange');
-      //NSNotificationCenter.defaultCenter().removeObserverName(self, 'PopupMenuOnNote');
+      //NSNotificationCenter.defaultCenter().removeObserverName(self,'ProcessNewExcerpt');
+      //NSNotificationCenter.defaultCenter().removeObserverName(self,'ChangeExcerptRange');
+      NSNotificationCenter.defaultCenter().removeObserverName(self, 'PopupMenuOnNote');
     },
     documentDidOpen: function(docmd5) {
     },
@@ -69,7 +98,7 @@ JSB.newAddon = function(mainPath){
       return null;
     },
     //Creating new note
-    onProcessNewExcerpt: function(sender){
+    /*onProcessNewExcerpt: function(sender){
       if(!Application.sharedInstance().checkNotifySenderInWindow(sender,self.window))return;//Don't process message from other window
       if(!self.autodef)return;
       let noteid = sender.userInfo.noteid;
@@ -118,36 +147,59 @@ JSB.newAddon = function(mainPath){
           }
         });
       }
-    },
-    /*onPopupMenuOnNote: function (sender) {
+    },*/
+    onPopupMenuOnNote: function (sender) {
         if (!Application.sharedInstance().checkNotifySenderInWindow(sender, self.window)) return;//Don't process message from other window
         if (!self.autodef) return;
         let note = sender.userInfo.note;
-        let titleText = note.noteTitle;
-        let excerptText = note.excerptText.replace('**', '');
-        let text = titleText || excerptText;
-        if (text && text.length) {
-            // Copy to Clipboard
-            let pasteBoard = UIPasteboard.generalPasteboard();
-            pasteBoard.string = text;
+        if (note) {
+          let timerCount = 0;
+          NSTimer.scheduledTimerWithTimeInterval(1, true, function (timer) {
+              let text = note.allNoteText();
+              UndoManager.sharedInstance().undoGrouping('AutoDef', note.notebookId, function () {
+                //自定义部分
+                let title=getGroupedDef(note.noteTitle,true);//处理原标题
+                let excerpt=getGroupedDef(note.noteTitle?text.substring(text.indexOf("\n") + 1):text,false);//处理下面的所有摘录
+                note.noteTitle=formatText(title,excerpt);//输出
+                //End
+                Database.sharedInstance().setNotebookSyncDirty(note.notebookId);
+              });
+              NSNotificationCenter.defaultCenter().postNotificationNameObjectUserInfo('RefreshAfterDBChange', self, {topicid: note.notebookId});
+
+              timerCount++;
+              if (timerCount >= 1) {
+                  timer.invalidate();
+              }
+          });
         }
-    },*/
+    },
     toggleAutoDef: function(sender) {
       var lan = NSLocale.preferredLanguages().length?NSLocale.preferredLanguages()[0].substring(0,2):'en';
+      let cnTips, enTips;
       if(self.autodef){
         self.autodef = false;
-        if(lan == 'zh')
-          Application.sharedInstance().showHUD('AutoDef已关闭',self.window,2);
-        else
-          Application.sharedInstance().showHUD('AutoDef off',self.window,2);
+        cnTips='AutoDef已关闭';
+        enTips='AutoDef disabled';
       }
       else{
         self.autodef = true;
-        if(lan == 'zh')
-          Application.sharedInstance().showHUD('AutoDef已打开',self.window,2);
-        else
-          Application.sharedInstance().showHUD('AutoDef On',self.window,2);
+        cnTips='点击笔记即可更新标题';
+        enTips='Now you can click on notes to update titles';
+        //目前无效
+        /*
+        if (self.autotitle || self.autotitle_with_excerpt || self.switchtitle) {
+          self.autotitle = false;
+          self.autotitle_with_excerpt = false;
+          self.switchtitle = false;
+          var list=self.autotitle?"autotitle,":''+
+            self.autotitle_with_excerpt?"autotitle_with_excerpt,":''+
+            self.switchtitle?"switchtitle,":'';
+          cnTips = `开启同时已关闭冲突插件：${list}`;
+          enTips = `The following conflict plugins are disabled: ${list}`;
+        }
+        */
       }
+      Application.sharedInstance().showHUD(lan === 'zh' ? cnTips : enTips, self.window, 2);
       NSUserDefaults.standardUserDefaults().setObjectForKey(self.autodef,'marginnote_autodef');
       Application.sharedInstance().studyController(self.window).refreshAddonCommands();
     },
